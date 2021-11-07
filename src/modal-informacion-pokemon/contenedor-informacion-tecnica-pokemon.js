3c2c7ac46945a209875265e8c2f61a6fdf514dd2
Franco Gomez <mgomez21699@gmail.com>
2021-11-07 07:55:51 -0300

    Dividido tanto .js como .css en dos archivos para aumentar la legibilidad del codigo.

--- src/modal-informacion-pokemon/contenedor-informacion-tecnica-pokemon.js
+++ src/modal-informacion-pokemon/contenedor-informacion-tecnica-pokemon.js
@@ -1,15 +1,6 @@
 const elementosLista = ["Biography", "Stats", "Evolutions"];
 
 const crearContenedorInformacionTecnicaPokemon = (pokemon, especiePokemon) => {
-  const dataPokemon = {
-    Species: especiePokemon.genera[7].genus.replace(" Pokémon", ""),
-    Height: `${pokemon.height / 10}m`,
-    Weight: `${(pokemon.weight / 10).toFixed(1)}kg`,
-    Abilities: pokemon.abilities,
-    Gender: `${especiePokemon.gender_rate !== -1 ?  `♂ ${100 - (especiePokemon.gender_rate / 8) * 100} ♀ ${(especiePokemon.gender_rate / 8) * 100}` : "Genderless"}`,
-    "Catch Rate": `${((especiePokemon.capture_rate / 255) * 100).toFixed(1)}%`,
-  };
-
   const $contenedorInformacionTecnicaPokemon = document.createElement("div");
   $contenedorInformacionTecnicaPokemon.className =
     "modal-informacion-pokemon__contenedor-informacion-tecnica-pokemon";
@@ -24,74 +15,10 @@ const crearContenedorInformacionTecnicaPokemon = (pokemon, especiePokemon) => {
     $listaOpcionesInformacion.appendChild($elementoLista);
   });
 
-  const $contenedorPokemonData = document.createElement("div");
-  $contenedorPokemonData.className =
-    "modal-informacion-pokemon__contenedor-pokemon-data";
-
-  const $tituloPokemonData = document.createElement("h4");
-  $tituloPokemonData.className = "titulo-pokemon-data";
-  $tituloPokemonData.innerText = "Pokemon Data";
-
-  const $descripcionPokemon = document.createElement("p");
-  $descripcionPokemon.className = "descripcion-pokemon";
-
-  let descripcionPokemon = "";
-
-  especiePokemon.flavor_text_entries.forEach((entrada) => {
-    if (entrada.language.name === "en") {
-      descripcionPokemon = entrada.flavor_text.replaceAll("\n", " ");
-    }
-  });
-
-  $descripcionPokemon.innerText = descripcionPokemon;
-
-  const $listaDataPokemon = document.createElement("ul");
-  $listaDataPokemon.className = "lista-data-pokemon";
-
-  Object.keys(dataPokemon).forEach((data) => {
-    const $elementoDataPokemon = document.createElement("li");
-    $elementoDataPokemon.className = "elementoDataPokemon";
-
-    const $tituloElemento = document.createElement("h5");
-    $tituloElemento.className = "titulo-elemento";
-    $tituloElemento.innerText = data;
-
-    $elementoDataPokemon.appendChild($tituloElemento);
-
-    if (data !== "Abilities") {
-      const $dataElemento = document.createElement("p");
-      $dataElemento.className = "data-elemento-parrafo";
-      $dataElemento.innerText = dataPokemon[data];
-
-      $elementoDataPokemon.appendChild($dataElemento);
-    } else {
-      const $dataElemento = document.createElement("ul");
-      $dataElemento.className = "data-elemento";
-
-      pokemon.abilities.forEach((slotAbilidad) => {
-        if (slotAbilidad.ability) {
-          const $itemDataElemento = document.createElement("li");
-          $itemDataElemento.className = "item-data-elemento";
-          $itemDataElemento.innerText =
-            slotAbilidad.ability.name[0].toUpperCase() +
-            slotAbilidad.ability.name.slice(1);
-
-          $dataElemento.appendChild($itemDataElemento);
-        }
-      });
-
-      $elementoDataPokemon.appendChild($dataElemento);
-    }
-
-    $listaDataPokemon.appendChild($elementoDataPokemon);
-  });
-
-  $contenedorPokemonData.appendChild($tituloPokemonData);
-  $contenedorPokemonData.appendChild($descripcionPokemon);
-  $contenedorPokemonData.appendChild($listaDataPokemon);
-
   $contenedorInformacionTecnicaPokemon.appendChild($listaOpcionesInformacion);
-  $contenedorInformacionTecnicaPokemon.appendChild($contenedorPokemonData);
+  $contenedorInformacionTecnicaPokemon.appendChild(
+    crearContenedorPokemonData(pokemon, especiePokemon)
+  );
 
   return $contenedorInformacionTecnicaPokemon;
 };
